42eab19d1990decd67a8e8ad681bd9e6fd9a084b
Equester517 <junohb135@gmail.com>
2024-09-27 10:17:01 +0900

    feat: ServiceCreator 소켓 통신 추가

--- src/eevp_main_machine/ap_service_creator/include/ServiceCreator.h
+++ src/eevp_main_machine/ap_service_creator/include/ServiceCreator.h
@@ -1,34 +1,42 @@
 #include <csignal>
 #include <thread>
 #include <ara/log/logger.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUF_SIZE 1024
 
-namespace eevp {
-namespace simulation {
+namespace eevp
+{
+    namespace simulation
+    {
 
-class ServiceCreator {
-public:
-    /// @brief Constructor
-    ServiceCreator();
-    /// @brief Destructor
-    ~ServiceCreator();
+        class ServiceCreator
+        {
+        public:
+            /// @brief Constructor
+            ServiceCreator();
+            /// @brief Destructor
+            ~ServiceCreator();
 
-    /// @brief Start S/W Component
-    bool Start();
-    /// @brief Run S/W Component
-    void Run();
-    /// @brief Terminate S/W Component
-    void Terminate();
+            /// @brief Start S/W Component
+            bool Start();
+            /// @brief Run S/W Component
+            void Run();
+            /// @brief Terminate S/W Component
+            void Terminate();
 
-private:
-    static void SignalHandler(std::int32_t signal);
-    
-    /// @brief set Running State
-    bool setRunningState();
+        private:
+            static void SignalHandler(std::int32_t signal);
 
-    static std::atomic_bool mRunning;
-    ara::log::Logger& mLogger;
+            /// @brief set Running State
+            bool setRunningState();
 
-};
+            static std::atomic_bool mRunning;
+            ara::log::Logger &mLogger;
+        };
 
-}   // namespace monitoring
-}   // namespace eevp
+    } // namespace monitoring
+} // namespace eevp
--- src/eevp_main_machine/ap_service_creator/src/ServiceCreator.cpp
+++ src/eevp_main_machine/ap_service_creator/src/ServiceCreator.cpp
@@ -3,68 +3,115 @@
 #include "ServiceCreator.h"
 #include "ara/log/logger.h"
 
-namespace eevp {
-namespace simulation {
-
-std::atomic_bool ServiceCreator::mRunning(false);
-
-ServiceCreator::ServiceCreator() :
-        mLogger(ara::log::CreateLogger("SVCC", "SVCC", ara::log::LogLevel::kInfo)){
-    mLogger.LogInfo() << __func__;
-    // std::signal(SIGINT, SignalHandler);
-    std::signal(SIGTERM, SignalHandler);
-}
-
-ServiceCreator::~ServiceCreator() {
-}
-
-void
-ServiceCreator::SignalHandler(std::int32_t /*signal*/) {
-    mRunning = false;
-}
-bool
-ServiceCreator::Start() {
-    mLogger.LogInfo() << __func__;
-    mRunning = true;
-
-    if (!setRunningState()) {
-        return false;
-    }
-    
-    return true;
-}
-
-void
-ServiceCreator::Run() {
-    mLogger.LogInfo() << __func__;
-
-    if (mRunning) 
-    {
-        mLogger.LogInfo() << "Service Creator running";
-        //std::this_thread::sleep_for(std::chrono::seconds(5));
-    }
-    
-}
-
-void
-ServiceCreator::Terminate() 
+namespace eevp
 {
-    mLogger.LogInfo() << __func__;
-    mRunning = false;
-}
+    namespace simulation
+    {
 
-bool
-ServiceCreator::setRunningState() {
-    ara::exec::ExecutionClient executionClient;
-    auto exec = executionClient.ReportExecutionState(ara::exec::ExecutionState::kRunning);
-    if (exec.HasValue()) {
-        mLogger.LogInfo() << "ServiceCreator app in Running State";
-    } else {
-        mLogger.LogError() << exec.Error().Message();
-        return false;
+        std::atomic_bool ServiceCreator::mRunning(false);
+
+        ServiceCreator::ServiceCreator() : mLogger(ara::log::CreateLogger("SVCC", "SVCC", ara::log::LogLevel::kInfo))
+        {
+            mLogger.LogInfo() << __func__;
+            // std::signal(SIGINT, SignalHandler);
+            std::signal(SIGTERM, SignalHandler);
+        }
+
+        ServiceCreator::~ServiceCreator()
+        {
+        }
+
+        void
+        ServiceCreator::SignalHandler(std::int32_t /*signal*/)
+        {
+            mRunning = false;
+        }
+        bool
+        ServiceCreator::Start()
+        {
+            mLogger.LogInfo() << __func__;
+            mRunning = true;
+
+            if (!setRunningState())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        void
+        ServiceCreator::Run()
+        {
+            mLogger.LogInfo() << __func__;
+
+            // while (mRunning)
+            // {
+            //     mLogger.LogInfo() << "Service Creator running";
+            //     std::this_thread::sleep_for(std::chrono::seconds(2));
+            // }
+            /*소켓 코드*/
+            int sock;
+            char message[BUF_SIZE];
+            int str_len;
+            struct sockaddr_in serv_adr;
+
+            sock = socket(PF_INET, SOCK_STREAM, 0);
+            if (sock == -1)
+                mLogger.LogInfo() << "socket() error";
+
+            memset(&serv_adr, 0, sizeof(serv_adr));
+            serv_adr.sin_family = AF_INET;
+            serv_adr.sin_addr.s_addr = inet_addr("192.168.100.242");
+            serv_adr.sin_port = htons(5000);
+
+            // 클라이언트가 서버와의 연결을 위해 연결요청을 한다.
+            if (connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
+                mLogger.LogInfo() << "connect() error!";
+            else
+                mLogger.LogInfo() << "Connected...........";
+
+            // 사용자가 q를 입력할 때까지 반복
+            for (int i = 0; ; i++)
+            {
+                std::string str = "hello" + std::to_string(i);
+                strcpy(message, str.c_str());
+
+                // 콘솔창에 출력하는 함수
+                str_len = read(sock, message, BUF_SIZE - 1);
+                mLogger.LogInfo() << message;
+                write(sock, message, strlen(message));
+                message[str_len] = 0;
+                mLogger.LogInfo() << "Message from server and send: " << message;
+                std::this_thread::sleep_for(std::chrono::seconds(2));
+            }
+            close(sock);
+            /*소켓 코드 완료*/
+        }
+
+        void
+        ServiceCreator::Terminate()
+        {
+            mLogger.LogInfo() << __func__;
+            mRunning = false;
+        }
+
+        bool
+        ServiceCreator::setRunningState()
+        {
+            ara::exec::ExecutionClient executionClient;
+            auto exec = executionClient.ReportExecutionState(ara::exec::ExecutionState::kRunning);
+            if (exec.HasValue())
+            {
+                mLogger.LogInfo() << "ServiceCreator app in Running State";
+            }
+            else
+            {
+                mLogger.LogError() << exec.Error().Message();
+                return false;
+            }
+            return true;
+        }
+
     }
-    return true;
-}
-
-}
 }
